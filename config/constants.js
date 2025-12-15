d4c6807eec229ee6d7315611f296c7419cfa0a40
gitsecondsss <gitsecondsss@gmx.us>
2025-12-15 23:06:28 +0200

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,3 @@
+export const TOKEN_TTL = 120; // 2 minutes
+export const TRUST_TOKEN_SECRET = process.env.TRUST_TOKEN_SECRET || "f01d0e879ef01d0e879ef01d0e879ef01d0e879ef01d0e879ef01d0e879ef01d0e879ef01d0e879ef01d0e879e";
+export const TURNSTILE_SECRET = process.env.TURNSTILE_SECRET || "0x4AAAAAACEAdSoSffFlw4Y93xBl0UFbgsc";
